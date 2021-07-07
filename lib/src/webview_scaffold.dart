63970681b0949712fe447fdd39626f5e9807610d
Logachev Nikita <logach3@gmail.com>
2021-07-07 20:53:32 +0300

    Update webview_scaffold.dart

--- lib/src/webview_scaffold.dart
+++ lib/src/webview_scaffold.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:developer';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -96,6 +97,8 @@ class _WebviewScaffoldState extends State<WebviewScaffold> {
       if (!mounted) {
         return;
       }
+      
+      log('webviewReference on back listen');
 
       // The willPop/pop pair here is equivalent to Navigator.maybePop(),
       // which is what's called from the flutter back button handler.
